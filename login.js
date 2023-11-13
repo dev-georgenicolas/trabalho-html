37add9bb903417e6fff3e0726c7b9cb381c63c65
George_Nicolas <149883721+NicolasQueite@users.noreply.github.com>
2023-11-13 15:38:20 -0300

    123

---
+++ login.js
@@ -0,0 +1,13 @@
+<script>
+    function logar() {
+        var login = document.getElementyById('Login').value;
+    
+        if (login == "wlawla")
+          alert('Sucesso') 
+
+    }
+
+
+
+
+</script>
