7b3d83452efa8b83015ddce7366bfec301e82abd
pedro_nepomuceno <149882667+penepo@users.noreply.github.com>
2023-12-04 16:36:18 -0300

    wswxw
    xwxw

--- login.js
+++ login.js
@@ -3,10 +3,10 @@
         var senha = document.getElementById('senha').value;
     
         if(login === "wlawla" && senha === "157"){   
-            // Redirect to "cards.html" if login and password match
+            
             window.location.href = "cards.html";
         } else {
-            // Show an alert if login is unknown
+            
             alert('Login desconhecido');
         }
     }
